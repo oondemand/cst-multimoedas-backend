f916d00a6e10c7f40020341d6e088ff35b604590
Maike <maikonalexandre574@gmail.com>
2025-08-06 10:27:34 -0300

    feat: add fields moeda, valorMoeda and valorTotal

--- src/models/Servico.js
+++ src/models/Servico.js
@@ -1,26 +1,37 @@
 const mongoose = require("mongoose");
 
-const servicoSchema = new mongoose.Schema({
-  tipoServicoTomado: String,
-  descricao: String,
-  valor: Number,
-  pessoa: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Pessoa",
-    required: [true, "Pessoa é obrigatório"],
+const servicoSchema = new mongoose.Schema(
+  {
+    tipoServicoTomado: String,
+    descricao: String,
+    valor: Number,
+    valorMoeda: Number,
+    moeda: String,
+    pessoa: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pessoa",
+      required: [true, "Pessoa é obrigatório"],
+    },
+    dataContratacao: Date,
+    dataConclusao: Date,
+    status: {
+      type: String,
+      enum: ["ativo", "inativo", "arquivado"],
+      default: "ativo",
+    },
+    statusProcessamento: {
+      type: String,
+      enum: ["aberto", "pendente", "processando", "pago", "pago-externo"],
+      default: "aberto",
+    },
   },
-  dataContratacao: Date,
-  dataConclusao: Date,
-  status: {
-    type: String,
-    enum: ["ativo", "inativo", "arquivado"],
-    default: "ativo",
-  },
-  statusProcessamento: {
-    type: String,
-    enum: ["aberto", "pendente", "processando", "pago", "pago-externo"],
-    default: "aberto",
-  },
+  {
+    virtuals: true,
+  }
+);
+
+servicoSchema.virtual("valorTotal").get(function () {
+  return this.valor * (this?.valorMoeda ?? 1);
 });
 
 module.exports = mongoose.model("Servico", servicoSchema);
