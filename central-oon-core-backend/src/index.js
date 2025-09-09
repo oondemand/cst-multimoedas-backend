9f8e7c028b0177e26a0bfdc1db2273c0dd9fb2b6
Fabio <90531115+fanaia@users.noreply.github.com>
2025-09-09 10:47:21 -0300

    fix: load env before db config

---
+++ central-oon-core-backend/src/index.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const createApp = require('./boot/createApp');
+const createServer = require('./boot/createServer');
+
+module.exports = {
+  createApp,
+  createServer,
+};
