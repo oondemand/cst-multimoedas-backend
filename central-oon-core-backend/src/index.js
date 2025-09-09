fa085eb86a2d064606f49809ab44b409c296caa5
Fabio <90531115+fanaia@users.noreply.github.com>
2025-09-09 11:23:14 -0300

    feat(core): export new utilities

--- central-oon-core-backend/src/index.js
+++ central-oon-core-backend/src/index.js
@@ -1,18 +1,17 @@
-
 const dotenv = require('dotenv');
 dotenv.config();
 
 const createApp = require('./boot/createApp');
 const createServer = require('./boot/createServer');
 const logger = require('./config/logger');
-const createHttpClient = require('./config/httpClient');
 const { uploadExcel, uploadPDF } = require('./config/multer');
+const createHttpClient = require('./config/httpClient');
 
 module.exports = {
   createApp,
   createServer,
   logger,
-  createHttpClient,
   uploadExcel,
   uploadPDF,
+  createHttpClient,
 };
