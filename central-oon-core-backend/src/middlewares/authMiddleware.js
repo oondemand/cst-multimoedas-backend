b1c898fa5c9149226d6c447468578cae1f0dda27
Fabio <90531115+fanaia@users.noreply.github.com>
2025-09-10 10:44:38 -0300

    feat: reuse core auth middleware

--- central-oon-core-backend/src/middlewares/authMiddleware.js
+++ central-oon-core-backend/src/middlewares/authMiddleware.js
@@ -4,51 +4,55 @@ const apiMeusApps = createHttpClient({
 });
 const { sendErrorResponse } = require('../utils/response');
 
-const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+function authMiddleware({ getOrigin } = {}) {
+  return async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
 
-  if (!token) {
-    return sendErrorResponse({
-      res,
-      statusCode: 401,
-      message: 'Acesso não autorizado. Token ausente.',
-    });
-  }
-
-  try {
-    const response = await apiMeusApps.get('/auth/autenticar-aplicativo/', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const usuario = {
-      tipo:
-        response.data.usuario.aplicativo.tipoAcesso === 'master'
-          ? 'admin'
-          : response.data.usuario.aplicativo.tipoAcesso,
-      nome: response.data.usuario.nome,
-      email: response.data.usuario.email,
-    };
-
-    req.usuario = usuario;
-    next();
-  } catch (error) {
-    if (error.response && error.response.status === 401) {
+    if (!token) {
       return sendErrorResponse({
         res,
         statusCode: 401,
-        message: 'Token inválido ou erro na autenticação externa.',
+        message: 'Acesso não autorizado. Token ausente.',
       });
     }
 
-    return sendErrorResponse({
-      res,
-      statusCode: 500,
-      message: 'Houve um erro ao autenticar o usuário.',
-      error: error.message,
-    });
-  }
-};
+    try {
+      const origin = getOrigin ? await getOrigin() : undefined;
+      const response = await apiMeusApps.get('/auth/autenticar-aplicativo/', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          ...(origin && { origin }),
+        },
+      });
+
+      const usuario = {
+        tipo:
+          response.data.usuario.aplicativo.tipoAcesso === 'master'
+            ? 'admin'
+            : response.data.usuario.aplicativo.tipoAcesso,
+        nome: response.data.usuario.nome,
+        email: response.data.usuario.email,
+      };
+
+      req.usuario = usuario;
+      next();
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        return sendErrorResponse({
+          res,
+          statusCode: 401,
+          message: 'Token inválido ou erro na autenticação externa.',
+        });
+      }
+
+      return sendErrorResponse({
+        res,
+        statusCode: 500,
+        message: 'Houve um erro ao autenticar o usuário.',
+        error: error.message,
+      });
+    }
+  };
+}
 
 module.exports = authMiddleware;
